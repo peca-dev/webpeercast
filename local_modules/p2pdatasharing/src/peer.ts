d6e0359483be93ae108e5b3e56bf677bb652e8c8
progre <djyayutto@gmail.com>
2017-02-03 08:49:32 +0900

    WIP

--- local_modules/p2pdatasharing/src/peer.ts
+++ local_modules/p2pdatasharing/src/peer.ts
@@ -1,14 +1,56 @@
+import { EventEmitter } from "fbemitter";
 import { LocalPeer } from "p2pcommunication";
 
 // キューを保持して、必要があれば送ったりする
-export default class Peer<T> {
+export default class Peer<T extends { id: string }> extends EventEmitter {
     private p2pPeer: LocalPeer;
-    // private eventQueue: T[] = [];
+    private eventQueue: Array<Query<T>> = [];
 
     constructor(url: string) {
-        this.p2pPeer = new LocalPeer(url);
-        this.p2pPeer.addListener("connected", () => {
+        super();
 
+        this.p2pPeer = new LocalPeer(url);
+        this.p2pPeer.addListener("broadcast", (data: Array<Query<T>>) => {
+            this.eventQueue.push(...data);
         });
     }
+
+    set(date: Date, data: T) {
+        this.p2pPeer.broadcast([{ type: "set", date, data }]);
+    }
+
+    delete(date: Date, id: string) {
+        this.p2pPeer.broadcast([{ type: "delete", date, data: { id } }]);
+    }
+
+    getAll() {
+        let map = new Map<string, T>();
+        for (let datum of this.eventQueue.concat().sort(asc)) {
+            switch (datum.type) {
+                case "set":
+                    map.set(datum.payload.id, datum.payload);
+                    break;
+                case "delete":
+                    map.delete(datum.payload.id);
+                    break;
+                default:
+                    throw new Error("Unsupported type.");
+            }
+        }
+        return Array.from(map.values());
+    }
+}
+
+interface Query<T extends { id: string }> {
+    type: "set" | "delete";
+    date: Date;
+    payload: T;
+}
+
+function asc(a: Query<any>, b: Query<any>) {
+    let aTime = a.date.getTime();
+    let bTime = b.date.getTime();
+    return aTime < bTime ? -1
+        : aTime > bTime ? 1
+            : 0;
 }
