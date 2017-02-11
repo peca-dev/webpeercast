5fd5202d1d74c7770a2573dac57f09e09b09227c
progre <djyayutto@gmail.com>
2017-02-11 15:04:57 +0900

    Add peercast-yp-channels-parser module

---
+++ local_modules/peercast-yp-channels-parser/index.d.ts
@@ -0,0 +1,48 @@
+export function stringify(channels: Channel[]): string;
+export function parse(body: string): Channel[];
+
+export interface Channel {
+    readonly name: string;
+    readonly id: string;
+    readonly ip: string;
+    readonly url: string;
+    readonly genre: string;
+    readonly desc: string;
+    readonly bandType: string;
+    readonly listeners: number;
+    readonly relays: number;
+    readonly bitrate: number;
+    readonly type: string;
+    readonly track: {
+        readonly creator: string;
+        readonly album: string;
+        readonly title: string;
+        readonly url: string;
+    };
+    readonly uptime: number;
+    readonly comment: string;
+    readonly direct: boolean;
+}
+
+export interface MutableChannel extends Channel {
+    name: string;
+    id: string;
+    ip: string;
+    url: string;
+    genre: string;
+    desc: string;
+    bandType: string;
+    listeners: number;
+    relays: number;
+    bitrate: number;
+    type: string;
+    track: {
+        creator: string;
+        album: string;
+        title: string;
+        url: string;
+    };
+    uptime: number;
+    comment: string;
+    direct: boolean;
+}
