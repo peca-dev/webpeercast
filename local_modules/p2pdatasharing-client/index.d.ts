5022a960c7495a0c807444b6d623998093dae3d1
progre <djyayutto@gmail.com>
2017-02-19 00:25:56 +0900

    Fix typ

--- local_modules/p2pdatasharing-client/index.d.ts
+++ local_modules/p2pdatasharing-client/index.d.ts
@@ -1,7 +1,7 @@
 import * as Rx from "rxjs";
 
 export declare class Peer<T> {
-    onUpdated: Rx.Observable;
+    onUpdated: Rx.Observable<{}>;
 
     constructor(url: string);
     disconnect(): void;
