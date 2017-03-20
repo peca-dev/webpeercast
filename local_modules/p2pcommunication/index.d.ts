ffb4ec8986a215b21934c0e0edb4c02867ddc048
progre <djyayutto@gmail.com>
2017-03-20 12:01:25 +0900

    Fix test

--- local_modules/p2pcommunication/index.d.ts
+++ local_modules/p2pcommunication/index.d.ts
@@ -1,12 +1,12 @@
 import * as http from 'http';
 import { Subscribable } from 'rxjs/Observable';
 
-export declare class RemoteClient<T> {
+export declare class RemotePeer<T> {
   broadcast(payload: T): void
 }
 
 export declare class RootServer<T> {
-  onConnected: Subscribable<RemoteClient<T>>;
+  onConnected: Subscribable<{ peerType: "upstream" | "otherStream" | "downstream"; remotePeer: RemotePeer<T>; }>;
 
   constructor(httpServer: http.Server);
 
