2139ebd40030ad8132f65c4aa45de9695321fa66
progre <djyayutto@gmail.com>
2017-03-25 20:50:20 +0900

    Fix for node

--- local_modules/p2pcommunication-client/src/printerror.ts
+++ local_modules/p2pcommunication-client/src/printerror.ts
@@ -1,4 +1,5 @@
 declare const process: any;
+declare const require: any;
 
 export function safe<T>(func: (e: T) => Promise<void>) {
   return (event: T) => func(event).catch((e: any) => printError(e));
@@ -11,16 +12,56 @@ export function printError(e: any) {
     return;
   }
   console.error(
-    (process == null ? e.toString() + '\n' : '')
+    (isFirefox() ? e.toString() + '\n' : '')
     + (<string>e.stack)
       .split('\n')
-      .map(x => x.replace(/(.*?)@(.*)/, '    at $1 ($2)'))
+      .map(x => !isFirefox() ? x : x.replace(/(.*?)@(.*)/, '    at $1 ($2)'))
+      .map((x) => {
+        if (isFirefox()) {
+          return x;
+        }
+        if (x.includes('webpack:/')) {
+          return x.replace(/\/.*webpack:\//, '');
+        }
+        const m = x.match(/at(.*?)([ (])(\/[^:]+?):(.+):(.+)/);
+        if (m == null) {
+          return x;
+        }
+        return x.replace(
+          /at(.*?)([ (])(\/[^:]+?):(.+):(.+)/,
+          `at$1$2${toRelativePath(m[3])}:$4:$5`,
+        );
+      })
       .map(color)
       .join('\n'),
   );
 }
 
+function toRelativePath(filePath: string) {
+  return require('path').relative(process.cwd(), filePath);
+}
+
+function isFirefox() {
+  if (typeof navigator !== 'object') {
+    return false;
+  }
+  return navigator.userAgent.toLowerCase().includes('firefox');
+}
+
 function color(x: string) {
+  if (!isFirefox()) {
+    if (
+      [
+        'internal/',
+        'node_modules/',
+        '(native)',
+      ]
+        .some(y => x.includes(y))
+    ) {
+      return `\u001b[90m${x}\u001b[39m`;
+    }
+    return x;
+  }
   if (
     [
       '__emitToSubscription',
