a661320f81afd7cf487263c6f4e67dcae7d12b7b
progre <djyayutto@gmail.com>
2017-04-02 12:25:16 +0900

    WIP

---
+++ local_modules/p2pcommunication-client/src/Client.ts
@@ -0,0 +1,34 @@
+import { Observable } from 'rxjs';
+
+/**
+ * LocalPeer is presented the peer of local machine.
+ * It has a few RemotePeerRepos.
+ * RemotePeerRepo is RemoteRootServer or RemoteUpstream.
+ * It has a few RemotePeers.
+ * RemotePeers is created by RemotePeerRepo.
+ * It has a function what broadcast to known remote peers.
+ */
+export default class Client<T> {
+  private readonly remoteRootServer: RemoteRootServer<T>;
+  private readonly remotePeers: Set<RemotePeer<T>>;
+  private readonly localPeer: LocalPeer<T>;
+
+  constructor() {
+    this.localPeer.subscribe(this.remoteRootServer.offered);
+  }
+
+  private addRemotePeer(remotePeer: RemotePeer<T>) {
+    this.localPeer.subscribe(remotePeer.offered);
+  }
+}
+
+class LocalPeer<T> {
+  subscribe(offered: Observable<{}>) {
+    offered.subscribe(
+      () => {
+        answer().subscribe()
+      },
+      e => console.error(e.stack || e),
+    );
+  }
+}
